136ea27e6f8e371e6dfc8aa51d16c8969e56f7c5
Zeeshah Shafique Butt <zeeshah.shafique@mindstormstudios.com>
2022-08-11 12:14:18 +0500

    added boundaries to the scene and set the up dynamically.

--- Assets/_Pong/Scripts/SceneSpawner.cs
+++ Assets/_Pong/Scripts/SceneSpawner.cs
@@ -22,5 +22,22 @@ public class SceneSpawner : MonoBehaviour
         _widthUnit = Camera.aspect * _heightUnit;
         
         Debug.Log("YUnit Size: " + _heightUnit + "\tXUnit Size: " + _widthUnit);
+        
+        SetBoundaries();
+    }
+
+    void SetBoundaries()
+    {
+        Boundaries[0].transform.position = new Vector3(0, _heightUnit);
+        Boundaries[0].transform.localScale = new Vector3(_widthUnit*2, 0.5f);
+        
+        Boundaries[1].transform.position = new Vector3(0, -_heightUnit);
+        Boundaries[1].transform.localScale = new Vector3(_widthUnit * 2, 0.5f);
+        
+        Boundaries[2].transform.position = new Vector3(_widthUnit, 0);
+        Boundaries[2].transform.localScale = new Vector3(0.5f, _heightUnit * 2);
+
+        Boundaries[3].transform.position = new Vector3(-_widthUnit, 0);
+        Boundaries[3].transform.localScale = new Vector3(0.5f, _heightUnit * 2);
     }
 }
